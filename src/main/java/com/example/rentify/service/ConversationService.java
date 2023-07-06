6002d5c3b5a8a94c7699724b210e28f97507ebf8
Heril Muratovic <heril.muratovic@glopal.com>
2023-07-06 14:03:06 +0200

    Lombok reverted

--- src/main/java/com/example/rentify/service/ConversationService.java
+++ src/main/java/com/example/rentify/service/ConversationService.java
@@ -5,31 +5,19 @@ import com.example.rentify.entity.RedisConversation;
 import com.example.rentify.ws.repository.RedisConversationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ConversationService {
 
     private final RedisConversationRepository redisConversationRepository;
-    private final RedisTemplate<String, Object> redisTemplate;
-    private final HashOperations<String, Long, RedisConversation> hashOperations;
-
-    public ConversationService(RedisTemplate<String, Object> redisTemplate,
-                               RedisConversationRepository redisConversationRepository) {
-        this.redisConversationRepository = redisConversationRepository;
-        this.redisTemplate = redisTemplate;
-        this.hashOperations = redisTemplate.opsForHash();
-    }
 
 
     public String create(String usernameFrom, String usernameTo) {
